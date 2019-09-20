8d4ef13eecfc4584865aa33040d610357fbee08b
cpodgell <curtisstodgell@hotmail.com>
2019-09-20 07:25:22 -0700

    end of section 7

--- DataApp-SPA/src/app/_services/auth.service.ts
+++ DataApp-SPA/src/app/_services/auth.service.ts
@@ -15,6 +15,7 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(model: any) {
+    console.log('FUC KTHIS!');
     return this.http.post(this.baseUrl + 'login', model).pipe(
       map((response: any) => {
         const user = response;
